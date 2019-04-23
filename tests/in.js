6eb7577e024feb8c6e95f5c103fd77d29dbde025
oakfang <oakfang@gmail.com>
2019-04-23 17:18:38 +0300

    Major rehaul

--- tests/in.js
+++ tests/in.js
@@ -1,22 +1,22 @@
-const Coven = require("coven");
 const wrtc = require("wrtc");
 const ws = require("ws");
-const { Spreader } = require("..");
+const Manager = require("..");
 
-const DEV = "ws://localhost:4000";
 const PROD = "wss://coven-broker.now.sh";
-const coven = new Coven({ wrtc, ws, signaling: PROD });
+const manager = new Manager({ wrtc, ws, signaling: PROD });
 
 const [file, password, tag] = process.argv.slice(2);
 
-const sub = new Spreader(file, password, tag, coven);
-sub.on("publishing", ({ blockNumber, peerId }) =>
-  console.log(`Publishing block #${blockNumber} to peer ${peerId}`)
-);
-sub.on("written", ({ blockNumber, credit }) =>
-  console.log(
-    `Writing to disk block #${blockNumber} published by peer ${credit}`
+manager
+  .download(tag, file, password)
+  .once("ready", tag => console.log(tag))
+  .on("publishing", ({ blockNumber, peerId }) =>
+    console.log(`Publishing block #${blockNumber} to peer ${peerId}`)
   )
-);
-sub.once("finish", () => console.log("File written to disk"));
-sub.start();
+  .on("written", ({ blockNumber, credit }) =>
+    console.log(
+      `Writing to disk block #${blockNumber} published by peer ${credit}`
+    )
+  )
+  .once("done", () => console.log("File written to disk"))
+  .start();
--- tests/out.js
+++ tests/out.js
@@ -1,17 +1,19 @@
-const Coven = require("coven");
 const wrtc = require("wrtc");
 const ws = require("ws");
-const { Publisher } = require("..");
+const Manager = require("..");
 
-const DEV = "ws://localhost:4000";
 const PROD = "wss://coven-broker.now.sh";
-const coven = new Coven({ wrtc, ws, signaling: PROD });
+const manager = new Manager({ wrtc, ws, signaling: PROD });
 
 const [file, password] = process.argv.slice(2);
 
-const pub = new Publisher(file, password, coven);
-pub.once("ready", () => console.log(pub.tagBase));
-pub.on("publishing", ({ blockNumber, peerId }) =>
-  console.log(`Publishing block #${blockNumber} to peer ${peerId}`)
-);
-pub.start();
+manager
+  .publish(file, password)
+  .once("ready", tag => console.log(tag))
+  .on("publishing", ({ blockNumber, peerId }) =>
+    console.log(`Publishing block #${blockNumber} to peer ${peerId}`)
+  )
+  .on("processing", ({ blockNumber }) =>
+    console.log(`Processing block #${blockNumber}`)
+  )
+  .start();
